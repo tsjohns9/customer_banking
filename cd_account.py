6593e28e31cc67b8770ff1617a1548128be502b3
Trevor Johnson <johnsontrevor55@gmail.com>
2024-06-18 23:21:47 -0700

    initial

---
+++ cd_account.py
@@ -0,0 +1,19 @@
+"""Import the Account class from the Account.py file."""
+
+from Account import Account
+
+
+def create_cd_account(balance, interest_rate, months):
+    """Creates a CD account, calculates interest earned, and updates the account balance.
+
+    Args:
+        balance (float): The initial CD account balance.
+        interest_rate (float): The APR interest rate for the CD account.
+        months (int): The length of months for the CD.
+
+    Returns:
+        float: The updated CD account balance after adding the interest earned.
+        And returns the interest earned.
+    """
+
+    pass
---
+++ customer_banking.py
@@ -0,0 +1,13 @@
+from cd_account import create_cd_account
+from savings_account import create_savings_account
+
+
+def main():
+    """This function prompts the user to enter the savings and cd account balance, interest rate,
+    and the length of months to determine the interest gained.
+    It displays the interest earned on the savings and CD accounts and updates the balances.
+    """
+
+
+if __name__ == "__main__":
+    main()
